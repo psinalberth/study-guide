2d0338a74a41a57c07e7adbcc7a1436c9906b502
Inalberth Pinheiro Santos <inalberth07@gmail.com>
2025-08-25 08:30:53 -0300

    improving code syntax for algorithms test

--- loadbalancing/src/app.js
+++ loadbalancing/src/app.js
@@ -5,7 +5,16 @@ const SERVER_NAME = process.env.SERVER_NAME || "App";
 const PORT = process.env.PORT || 3000;
 const ALGORITHM = process.env.ALGORITHM || "round-robin";
 
-const hello = (res) => {
+const algorithms = {
+  'least-conn': (res, delay) => {
+    setTimeout(() => greetings(res), delay);
+  },
+  default: (res, _) => {
+    greetings(res)
+  }
+};
+
+const greetings = (res) => {
   res.send({
     server: SERVER_NAME,
     message: `Hello from server ${SERVER_NAME}.`,
@@ -14,15 +23,14 @@ const hello = (res) => {
   });
 };
 
+
 app.get("", (req, res) => {
   const delay = req.query.delay;
-  const server = req.query.server;
   console.log(
-    `Request received at server ${SERVER_NAME} with delay=${delay} and server=${server}`
+    `Request received at server ${SERVER_NAME} with delay=${delay}`
   );
-  (delay && "#" + server === SERVER_NAME) || ALGORITHM === "least-conn"
-    ? setTimeout(() => hello(res), delay)
-    : hello(res);
+    const alg = algorithms[ALGORITHM] || algorithms.default;
+    alg(res, delay);
 });
 
 app.listen(PORT, () => {
