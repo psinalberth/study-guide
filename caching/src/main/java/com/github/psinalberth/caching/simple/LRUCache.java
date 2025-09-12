a2d0b04d569389acec5ab1b759c9d8cbb2caa890
psinalberth <inalberth@gmail.com>
2025-09-12 07:38:14 -0300

    Implementing LRU cache

--- caching/src/main/java/com/github/psinalberth/caching/simple/LRUCache.java
+++ caching/src/main/java/com/github/psinalberth/caching/simple/LRUCache.java
@@ -1,4 +1,53 @@
 package com.github.psinalberth.caching.simple;
 
-public class LRUCache {
+import java.util.*;
+
+public class LRUCache<K, V> {
+
+    private final int capacity;
+    private final Map<K, V> cache;
+    private final LinkedList<K> keyList;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.keyList = new LinkedList<>();
+    }
+
+    public void put(K key, V value) {
+
+        if (keyList.contains(key)) {
+            keyList.remove(key);
+            cache.remove(key);
+        }
+
+        cache.put(key, value);
+        keyList.addFirst(key);
+
+        if (cache.size() > capacity) {
+            K lastElementKey = keyList.removeLast();
+            cache.remove(lastElementKey);
+        }
+    }
+
+    public V get(K key) {
+
+        if (!keyList.contains(key)) {
+            return null;
+        }
+
+        keyList.remove(key);
+        keyList.addFirst(key);
+
+        return cache.get(key);
+    }
+
+    @Override
+    public String toString() {
+        return "LRUCache{" +
+                "capacity=" + capacity +
+                ", cache=" + cache +
+                ", keyList=" + keyList +
+                '}';
+    }
 }
--- caching/src/test/java/com/github/psinalberth/caching/simple/LRUCacheTest.java
+++ caching/src/test/java/com/github/psinalberth/caching/simple/LRUCacheTest.java
@@ -1,7 +1,45 @@
 package com.github.psinalberth.caching.simple;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LRUCacheTest {
 
+    LRUCache<Integer, String> cache = new LRUCache<>(3);
+
+    @BeforeEach
+    void beforeEach() {
+        cache.put(1, "A");
+        cache.put(2, "B");
+        cache.put(3, "C");
+    }
+
+    @Test
+    @DisplayName("It should initialize cache structure successfully")
+    void shouldInitializeCache() {
+
+        assertThat(cache)
+                .isNotNull()
+                .returns("A", lruCache -> lruCache.get(1))
+                .returns("B", lruCache -> lruCache.get(2))
+                .returns("C", lruCache -> lruCache.get(3));
+    }
+
+    @Test
+    @DisplayName("It should put values to cache")
+    void shouldGetCacheKeys() {
+
+        cache.put(4, "D");
+        cache.put(3, "F");
+
+        assertThat(cache)
+                .isNotNull()
+                .returns("D", lruCache -> lruCache.get(4))
+                .returns("F", lruCache -> lruCache.get(3))
+                .returns("B", lruCache -> lruCache.get(2))
+                .returns(null, lruCache -> lruCache.get(1));
+    }
 }
