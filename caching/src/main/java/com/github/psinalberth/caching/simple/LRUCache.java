5f2a624cec77564c45d1ffd8d744ca273c2cd08c
psinalberth <inalberth@gmail.com>
2025-09-12 07:31:09 -0300

    Implementing LRU cache

---
+++ caching/src/main/java/com/github/psinalberth/caching/simple/LRUCache.java
@@ -0,0 +1,4 @@
+package com.github.psinalberth.caching.simple;
+
+public class LRUCache {
+}
---
+++ caching/src/test/java/com/github/psinalberth/caching/simple/LRUCacheTest.java
@@ -0,0 +1,7 @@
+package com.github.psinalberth.caching.simple;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LRUCacheTest {
+
+}
